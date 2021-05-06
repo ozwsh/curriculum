908bc24f75748ed9ea784f8b6430d39bfa026e7c
ozwsh <83494626+ozwsh@users.noreply.github.com>
2021-05-06 17:33:36 +0900

    課題提出
    Task1_1.javaの作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,34 @@
+/**
+ * Task1_1: 課題内容
+ *
+ * 本課題では、データ型・文字列の使い方を学んでいきましょう。
+ * 問①から問⑧まであります。
+ * 指定された型・値・変数名を守って記述してください。
+ * 問③,⑦については、コメントを入れてください。
+ *
+ */
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        // [「double型」の「hogeDouble」という変数の初期値が「0.2」]
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+
+        // [「Stringクラス」の「hogeString」という変数の値「合格」を表示]
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+
+
+    }
+}
