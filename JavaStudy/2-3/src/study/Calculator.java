ab8d347e221e55bdd54a8d1f8db18ef44f80d78b
ozwsh <83494626+ozwsh@users.noreply.github.com>
2021-06-25 10:51:50 +0900

    課題提出

---
+++ JavaStudy/2-3/src/study/Calculator.java
@@ -0,0 +1,19 @@
+package study;
+
+public class Calculator {
+
+    int plus;
+	
+	protected int plus(int a) {
+        return a + 1;
+    }
+
+    protected int plus(int a, int b) {
+        return a + b;
+    }
+
+    protected int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
